8e4fdf1548f342f9f6ea810a2c3aa21dd205cd97
aaaaaua <simferua@gmail.com>
2019-11-04 20:50:04 +0300

    Добавляет обработчик отправки формы, функцию перевода страницы в неактивное состояние, обработчик открытия-закрытия сообщений об успешной отправке данных формы, об ошибке отправки формы

--- js/backend.js
+++ js/backend.js
@@ -25,5 +25,24 @@ window.backend = (function () {
       xhr.open('GET', URL);
       xhr.send();
     },
+
+    save: function (URL, data, onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    }
   };
 })();
--- js/map.js
+++ js/map.js
@@ -12,9 +12,14 @@
   var map = document.querySelector('.map');// copy
   var mapFiltersForm = document.querySelector('.map__filters');
   var adForm = document.querySelector('.ad-form');
+  var formFieldsets = adForm.querySelectorAll('fieldset');
+
+  var MAIN_PIN_START_COORDS = 'left: 570px; top: 375px;';
 
   var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
+  var SAVE_URL = 'https://js.dump.academy/keksobooking';
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
 
   var onLoadOffers = function (pinOffers) {
     window.makePin(pinOffers);
@@ -24,12 +29,23 @@
   var onError = function () {
     var loadError = errorTemplate.cloneNode(true);
     map.appendChild(loadError);
+    window.errorButton = document.querySelector('.error__button');
   };
 
-  var makeFormElementsActive = function (form) {
-    var formFieldsets = form.querySelectorAll('fieldset');
+  var onSuccessSubmit = function () {
+    var successSubmit = successTemplate.cloneNode(true);
+    map.appendChild(successSubmit);
+  };
+
+  var makeFormElementsActive = function () {
     for (var i = 0; i < formFieldsets.length; i++) {
       formFieldsets[i].removeAttribute('disabled');
+    }
+  };
+
+  var makeFormElementsDisabled = function () {
+    for (var i = 0; i < formFieldsets.length; i++) {
+      formFieldsets[i].setAttribute('disabled', 'disabled');
     }
   };
 
@@ -38,6 +54,25 @@
     adForm.classList.remove(AD_FORM_DISABLED_CLASS);
     mapFiltersForm.classList.remove(MAP_FILTER_DISABLED_CLASS);
     makeFormElementsActive(adForm);
+  };
+
+  var removeMapPin = function () {
+    var pins = map.querySelectorAll('.map__pin');
+    for (var i = 0; i < pins.length; i++) {
+      if (!pins[i].classList.contains('map__pin--main')) {
+        pins[i].remove();
+      }
+    }
+  };
+
+  var makePageDisabled = function () {
+    removeMapPin();
+    adForm.reset();
+    map.classList.add(MAP_FADED_CLASS);
+    adForm.classList.add(AD_FORM_DISABLED_CLASS);
+    mapFiltersForm.classList.add(MAP_FILTER_DISABLED_CLASS);
+    window.mapPinMain.style.cssText = MAIN_PIN_START_COORDS;
+    makeFormElementsDisabled();
   };
 
   window.mapPinMain.addEventListener('mousedown', function () {
@@ -58,6 +93,7 @@
     }
   });
 
+  // Открытие карточки объявления
   map.addEventListener('click', window.openCardOffer);
   map.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
@@ -65,6 +101,7 @@
     }
   });
 
+  // Закрытие карточки объявления
   map.addEventListener('click', function (evt) {
     if (evt.target.className !== 'popup__close') {
       return;
@@ -76,6 +113,51 @@
     var offerCard = document.querySelector('.map__card');
     if (evt.keyCode === ESC_KEYCODE && offerCard) {
       offerCard.remove();
+    }
+  });
+
+  // Отправка своего объявления на сревер
+  adForm.addEventListener('submit', function (evt) {
+    var offerCard = document.querySelector('.map__card');
+    window.backend.save(SAVE_URL, new FormData(adForm), function (response) {
+      if (response) {
+        makePageDisabled();
+        onSuccessSubmit();
+      }
+      if (offerCard) {
+        offerCard.remove();
+      }
+    }, onError);
+    evt.preventDefault();
+  });
+
+  // Закрытие сообщения об успешной загрузке объявления на сервер
+  document.addEventListener('click', function () {
+    var successMessage = document.querySelector('.success');
+    if (successMessage) {
+      successMessage.remove();
+    }
+  });
+
+  document.addEventListener('keydown', function (evt) {
+    var successMessage = document.querySelector('.success');
+    if (evt.keyCode === ESC_KEYCODE && successMessage) {
+      successMessage.remove();
+    }
+  });
+
+  // Закрытие сообщения об ошибке при загрузке объявления на сервер
+  document.addEventListener('click', function () {
+    var errorMessage = document.querySelector('.error');
+    if (errorMessage) {
+      errorMessage.remove();
+    }
+  });
+
+  document.addEventListener('keydown', function (evt) {
+    var errorMessage = document.querySelector('.error');
+    if (evt.keyCode === ESC_KEYCODE && errorMessage) {
+      errorMessage.remove();
     }
   });
 })();
